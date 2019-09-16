32f7c5c81be86ea098cc86376b1587df607c6290
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-09-16 16:37:30 -0300

    Agrego clases nodo y lista, interfases.

---
+++ AED1-UT04-Prep/UT04CasoBase01/src/ut04casobase01/UT04CasoBase01.java
@@ -0,0 +1,23 @@
+/*
+ * MAIN
+ */
+package ut04casobase01;
+
+/**
+ * @author EQUIPO
+ */
+public class UT04CasoBase01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Creando nodos.
+        
+        
+        
+        
+    }
+    
+}
