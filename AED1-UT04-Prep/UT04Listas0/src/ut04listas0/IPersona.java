4f30fa60ab4cde6aa1f08e7b034d47fe6c8ed691
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-09-17 09:50:31 -0300

    .
    .

---
+++ AED1-UT04-Prep/UT04Listas0/src/ut04listas0/IPersona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut04listas0;
+
+/**
+ *
+ * @author EQUIPO
+ */
+public interface IPersona {
+
+    String getApellido();
+
+    String getNombre();
+
+    void setApellido(String apellido);
+
+    void setNombre(String nombre);
+    
+}
