49989c11f1746c3141bc2d5f429f2d226ebcb5a8
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-09-23 21:40:29 -0300

    UTListas

--- AED1-UT04/UTListas/src/utlistas/UTListas.java
+++ AED1-UT04/UTListas/src/utlistas/UTListas.java
@@ -14,7 +14,26 @@ public class UTListas {
      */
     public static void main(String[] args) {
         
-        // TODO code application logic here
+        // Crear lista.
+        ILista<Persona> lista = new Lista<>();
+        
+        // Crear personas.
+        Persona per1 = new Persona("Marcelo", "Stabile", "1");
+        Persona per2 = new Persona("Juan", "Perez", "2");
+        Persona per3 = new Persona("Jorge", "Lopez", "3");
+        
+        // Crear nodos.
+        INodo<Persona> nodoP1 = new Nodo<>(per1.getId(), per1);
+        INodo<Persona> nodoP2 = new Nodo<>(per2.getId(), per2);
+        INodo<Persona> nodoP3 = new Nodo<>(per3.getId(), per3);
+        
+        // Insertar nodos en la lista.
+        lista.insertarNodoOrdenado(nodoP1);
+        lista.insertarNodoOrdenado(nodoP2);
+        lista.insertarNodoOrdenado(nodoP3);
+        
+        //Imprimir por pantalla.
+        lista.imprimir();
+        
     }
-    
 }
