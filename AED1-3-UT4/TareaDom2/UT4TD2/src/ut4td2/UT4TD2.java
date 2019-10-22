a0a76819c50b29dd15eb3888c7f9eea117557b62
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-10-22 16:46:20 -0300

    .
    .

--- AED1-3-UT4/TareaDom2/UT4TD2/src/ut4td2/UT4TD2.java
+++ AED1-3-UT4/TareaDom2/UT4TD2/src/ut4td2/UT4TD2.java
@@ -25,23 +25,24 @@ public class UT4TD2 {
         IManejadorArchivosGenerico ManejadorArchivos = new ManejadorArchivosGenerico();
         
         // Archivos.
-        String CarpetaDeTrabajo = "src/UT4TD2/";
-        String arch1 = "Entrada.txt";
+        String CarpetaDeTrabajo = "src/ut4td2/";
+        String arch1 = "claves1.txt";
         
         // Crear árbol binario.
-        IArbolBB<String> arbolClaves = new TArbolBB<>();
+        TArbolBB<String> arbolClaves = new TArbolBB<>();
         
         // Cargar datos en el árbol binario.
         String[] lineasArchivo1 = ManejadorArchivos.leerArchivo(CarpetaDeTrabajo + arch1);
         for (String linea : lineasArchivo1) {
             String[] c = linea.split("");
-            IElementoAB<String> elementoNuevo = new TElementoAB(c[0], c[0]);
+            TElementoAB<String> elementoNuevo = new TElementoAB(c[0], c[0]);
             arbolClaves.insertar(elementoNuevo);
         }
         
+        // 
+        IElementoAB aux = arbolClaves.buscar("10197");
         
-        
-        
+        System.out.println(aux);
         
 
 
